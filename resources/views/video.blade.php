392f04f6acc0cb683840c70ab2d0532532c0ee96
xu <xufeng1023@hotmail.com>
2018-03-17 23:55:34 -0400

    'ff'

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -24,6 +24,7 @@
                 <video></video>
             </div>
         </div>
+        <script src="{{ asset('js/app.js') }}"></script>
         <script>
             // Older browsers might not implement mediaDevices at all, so we set an empty object first
 if (navigator.mediaDevices === undefined) {
